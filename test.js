efb3eae98511ba734fb356b349e32544a27e8f59
iamliormalichi <malichispam@gmail.com>
2022-01-01 00:45:20 +0200

    my first commit

---
+++ test.js
@@ -0,0 +1,74 @@
+
+
+// var n = "node test.js";
+
+// const array = [1,23,4,5];
+// const mapArray = array.map(num =>num*3);
+// const filterArray = array.filter(num => num%2 ===0)
+// const reduceArray = array.reduce((acc, num) => {return acc+num}, 0);
+// console.log(reduceArray);
+// console.log(array);
+// console.log(mapArray);
+// console.log(filterArray);
+
+
+// class Player{
+//     constructor(name, type){
+//         console.log(this)
+//         this.name = name;
+//         this.type = type;
+//     }
+//     intrsuce(){
+//         console.log(`hi im ${this.name} and im a ${this}`, this);
+//     } 
+// }
+
+// class Wizard extends Player{
+//     constructor(name, type){
+//         super(name, type);
+//         console.log("wizard", this);
+//     }
+//     play(){
+//         console.log("weeeee im a" ,this);
+//     }
+// }
+
+// const wizard1 = new Wizard("shelly", "healer"); 
+// wizard1.intrsuce();
+// wizard1.play();
+
+// const a = [1,2,3,4];
+
+// const b = Object.assign([], a);
+
+// console.log(a);
+// console.log(b);
+// console.log(a==b);
+
+// const obj={
+//     user1: "gandlof",
+//     user2: "naknash",
+//     user3: "maomer"
+// }
+// console.log("1");
+// console.log(
+//     Object.entries(obj));
+// console.log("2");
+// console.log(
+//     Object.entries(obj).map(value => value[1] + value[0].replace("user","")));
+// console.log("3");
+
+
+
+// let arr = ["1","2","3","1"];
+// let i =0;
+// for (i=0 ; i<arr.length; i++){
+//     arr[i] = arr[i].replace("1","3");
+// }
+// console.log(arr);
+
+
+
+let input = "adfaa@gmail.com";
+
+console.log(input[0]);
